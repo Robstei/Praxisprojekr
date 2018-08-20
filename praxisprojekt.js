1b7ee7873328e7f16a278ce3ce10d10706e1d65f
Robstei <robin.steil@gmx.de>
2018-08-20 23:22:39 +0200

    website für PP

--- praxisprojekt.js
+++ praxisprojekt.js
@@ -1,6 +1,5 @@
 function runsChanged() {
-    document.getElementById("run-amount").value;
-    for (var i = 0; i < document.getElementById("run-amount").value; i++) {
+    /* for (var i = 0; i < document.getElementById("run-amount").value; i++) {
         document.getElementById("run" + (i + 1)).style.display = "inline-block";
         document.getElementById("run-label" + (i + 1)).style.display = "inline-block";
     }
@@ -8,5 +7,32 @@ function runsChanged() {
     for (var i = Number(document.getElementById("run-amount").value) + 1; i <= 10; i++) {
         document.getElementById("run" + i).style.display = "none";
         document.getElementById("run-label" + i).style.display = "none";
+    } 
+
+     */
+
+    for (var i = 0; i < document.getElementById("run-amount").value; i++) {
+        document.getElementById("button-run" + (i + 1)).style.display = "inline-block";
     }
+
+    var tabs = document.getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    
+    document.getElementById("tab-run1").style.display = "inline-block";
+
+    for (var i = Number(document.getElementById("run-amount").value) + 1; i <= 10; i++) {
+        document.getElementById("button-run" + i).style.display = "none";
+    }
+}
+
+function openRun(tab) {
+    var tabs = document.getElementsByClassName("tab");
+
+    for (var i = 0; i < tabs.length; i++) {;
+        tabs[i].style.display = "none";
+    }
+
+    document.getElementById(tab).style.display = "block";
 }
